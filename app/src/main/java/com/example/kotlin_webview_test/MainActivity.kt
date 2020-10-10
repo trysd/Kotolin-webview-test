6ea1edd3d970cebac11db31a1f7ad35666dda13f
trysd <itrysd@icloud.com>
2020-10-10 11:50:04 +0900

    added webview

--- app/src/main/java/com/example/kotlin_webview_test/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_webview_test/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.example.kotlin_webview_test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.WindowManager
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myWebView = findViewById(R.id.webview) as WebView
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.settings.domStorageEnabled = true
+        myWebView.settings.setGeolocationEnabled(true)
+        myWebView.clearCache(true)
+
+        // Hardware Acceleration ON
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
+        );
+        myWebView.loadUrl("https://trysd.github.io/202010041506-amp-ang10-comp/")
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        val myWebView = findViewById(R.id.webview) as WebView
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+
 }
