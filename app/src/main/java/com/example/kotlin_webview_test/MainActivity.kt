1d6711b9fdfa89052326eeb87d6613856f325eff
trysd <itrysd@icloud.com>
2020-10-10 21:42:08 +0900

    added start progress bar

--- app/src/main/java/com/example/kotlin_webview_test/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_webview_test/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.kotlin_webview_test
 
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.KeyEvent
+import android.view.View
+import android.view.ViewGroup
 import android.view.WindowManager
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -12,7 +17,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
         val myWebView = findViewById(R.id.webview) as WebView
+
         myWebView.settings.javaScriptEnabled = true
         myWebView.settings.domStorageEnabled = true
         myWebView.settings.setGeolocationEnabled(true)
@@ -20,10 +27,23 @@ class MainActivity : AppCompatActivity() {
 
         // Hardware Acceleration ON
         getWindow().setFlags(
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
-                WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
         );
-        myWebView.loadUrl("https://trysd.github.io/202010041506-amp-ang10-comp/")
+
+        var urlStr = "https://trysd.github.io/202010041506-amp-ang10-comp/";
+        myWebView.loadUrl(urlStr)
+
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                super.onPageStarted(view, url, favicon)
+                progressBar.visibility = View.VISIBLE
+            }
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                progressBar.visibility = View.GONE
+            }
+        }
     }
 
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
